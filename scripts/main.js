c956c0614a595071a0e812d62445139e96b77811
Koblik Oleksandr <alex0teddy@gmail.com>
2022-07-28 14:32:41 +0300

    HW3 done

---
+++ scripts/main.js
@@ -0,0 +1,502 @@
+// 1=========================================================================================
+
+// let str = 'test string';
+// let num = 123;
+// let und = undefined;
+// let bool = true;
+// let nul = null;
+// let bInt = BigInt('123123');
+// let symb = Symbol('123123');
+// let testObj = {a: 1};
+
+// 2==========================================================================================
+
+// const firstVariable = 1;
+// console.log(firstVariable);
+//
+// let secondVariable = 2;
+// secondVariable = 4;
+// console.log(secondVariable, 'secondVariable');
+//
+// var thirdVariable = 3;
+// thirdVariable = 5;
+// console.log(thirdVariable, 'thirdVariable');
+
+// 3==========================================================================================
+
+// строка
+// let str = "36457";
+// console.log(typeof str);
+// console.log(str);
+//
+// let one = +str;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!str;
+// console.log(typeof two);
+// console.log(two);
+
+// число
+// let num = 29847;
+// console.log(typeof num);
+// console.log(num);
+//
+// let one = String(num);
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!num;
+// console.log(typeof two);
+// console.log(two);
+
+// undefined
+// let und = undefined;
+// console.log(typeof und);
+// console.log(und);
+//
+// let one = +und;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(und);
+// console.log(typeof two);
+// console.log(two);
+//
+// let three = !!und;
+// console.log(typeof three);
+// console.log(three);
+
+// null
+// let nul = null;
+// console.log(typeof nul);
+// console.log(nul);
+//
+// let one = +nul;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(nul);
+// console.log(typeof two);
+// console.log(two);
+//
+// let three = !!nul;
+// console.log(typeof three);
+// console.log(three);
+
+//boolean
+// let bool = true;
+// console.log(typeof bool);
+// console.log(bool);
+//
+// let one = +bool;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(bool);
+// console.log(typeof two);
+// console.log(two);
+
+//BigInt
+// let bInt = BigInt("3992836");
+// console.log(typeof bInt);
+// console.log(bInt);
+//
+// let one = String(bInt);
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!bInt;
+// console.log(typeof two);
+// console.log(two);
+
+//Symbol
+// let symb = Symbol("4556");
+// console.log(typeof symb);
+// console.log(symb);
+//
+// let one = String(symb);
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = !!symb;
+// console.log(typeof two);
+// console.log(two);
+
+//Object
+// let obj = {a: 2};
+// console.log(typeof obj);
+// console.log(obj);
+//
+// let one = +obj;
+// console.log(typeof one);
+// console.log(one);
+//
+// let two = String(obj);
+// console.log(typeof two);
+// console.log(two);
+//
+// let three = !!obj;
+// console.log(typeof three);
+// console.log(three);
+
+// 4==========================================================================================
+
+// строка
+
+// let str0 = 'test string';
+// let str00 = 'test string';
+// let strStr1 = str0 + str00;
+// console.log(strStr1);
+//
+// let str = 'test string';
+// let num = 123;
+// let strNum = str + num;
+// console.log(strNum);
+//
+// let str1 = 'test string';
+// let und = undefined;
+// let str1Und = str1 + und;
+// console.log(str1Und);
+//
+// let str2 = 'test string';
+// let bool = true;
+// let str2Bool = str2 + bool;
+// console.log(str2Bool);
+//
+// let str3 = 'test string';
+// let nul = null;
+// let str3Nul = str3 + nul;
+// console.log(str3Nul);
+//
+// let str4 = 'test string';
+// let bInt = BigInt('123123');
+// let str4BInt = str4 + bInt;
+// console.log(str4BInt);
+//
+// // let str5 = 'test string';
+// // let sym = Symbol('123123');
+// // let str5Sym = str5 + sym;
+// // console.log(str5Sym);
+//
+// let str6 = 'test string';
+// let testObj = {a: 1};
+// let str6TestObj = str6 + testObj;
+// console.log(str6TestObj);
+
+// число
+
+// let num = 123;
+// let numb = 100;
+// let numNumb = num + numb;
+// console.log(numNumb);
+//
+// let num0 = 123;
+// let str0 = 'test string';
+// let numStr = num0 + str0;
+// console.log(numStr);
+//
+// let num1 = 123;
+// let und1 = undefined;
+// let num1Und = num1 + und1;
+// console.log(num1Und);
+//
+// let num2 = 123;
+// let bool1 = true;
+// let num2Bool = num2 + bool1;
+// console.log(num2Bool);
+//
+// let num3 = 123;
+// let nul1 = null;
+// let num3Nul = num3 + nul1;
+// console.log(num3Nul);
+//
+// // let num4 = 123;
+// // let bInt1 = BigInt('123123');
+// // let num4BInt = num4 + bInt1;
+// // console.log(num4BInt);
+// //
+// // let num5 = 123;
+// // let sym = Symbol('123123');
+// // let num5Sym = num5 + sym;
+// // console.log(num5Sym);
+//
+// let num6 = 123;
+// let testObj1 = {a: 1};
+// let num6TestObj = num6 + testObj1;
+// console.log(num6TestObj);
+
+// 5==========================================================================================
+
+
+// строка
+
+
+// let str0 = 'test string';
+// let str00 = 'test string';
+// let strStr0 = str0 - str00;
+// console.log(strStr0);
+//
+// let str = 'test string';
+// let num = 123;
+// let strNum = str - num;
+// console.log(strNum);
+//
+// let str1 = 'test string';
+// let und = undefined;
+// let str1Und = str1 - und;
+// console.log(str1Und);
+//
+// let str2 = 'test string';
+// let bool = true;
+// let str2Bool = str2 - bool;
+// console.log(str2Bool);
+//
+// let str3 = 'test string';
+// let nul = null;
+// let str3Nul = str3 - nul;
+// console.log(str3Nul);
+//
+// //let str4 = 'test string';
+// //let bInt = BigInt('123123');
+// //let str4BInt = str4 - bInt;
+// //console.log(str4BInt);
+//
+// //let str5 = 'test string';
+// //let sym = Symbol('123123');
+// //let str5Sym = str5 - sym;
+// //console.log(str5Sym);
+//
+// let str6 = 'test string';
+// let testObj = {a: 1};
+// let str6TestObj = str6 - testObj;
+// console.log(str6TestObj);
+
+// число
+
+// let num00 = 123;
+// let str0 = 'test string';
+// let numStr = num00 - str0;
+// console.log(numStr);
+//
+// let num0 = 123;
+// let num000 = 100;
+// let numNum3 = num0 - num000;
+// console.log(numNum3);
+//
+// let num1 = 123;
+// let und1 = undefined;
+// let num1Und = num1 - und1;
+// console.log(num1Und);
+//
+// let num2 = 123;
+// let bool1 = true;
+// let num2Bool = num2 - bool1;
+// console.log(num2Bool);
+//
+// let num3 = 123;
+// let nul1 = null;
+// let num3Nul = num3 - nul1;
+// console.log(num3Nul);
+//
+// // let num4 = 123;
+// // let bInt1 = BigInt('123123');
+// // let num4BInt = num4 - bInt1;
+// // console.log(num4BInt);
+// //
+// // let num5 = 123;
+// // let sym = Symbol('123123');
+// // let num5Sym = num5 - sym;
+// // console.log(num5Sym);
+//
+// let num6 = 123;
+// let testObj1 = {a: 1};
+// let num6TestObj = num6 - testObj1;
+// console.log(num6TestObj);
+
+// 6==========================================================================================
+
+// let login = prompt(`wright login`);
+// let password = prompt(`write password`);
+// let loginPassword = login + password;
+// alert(loginPassword);
+
+// if (loginPassword === `kykold1234asdQQ`) {
+// 	alert(`Добро пожаловать.`);
+// } else {
+// 	alert(`Ошибка входа`);
+// }
+
+// 7==========================================================================================
+
+// let year = +prompt("Year of Birthday:");
+// let age = 2022 - year;
+//  	if (age >= 16) {
+//  		console.log(age, `Добро пожаловать.`);
+//  	} else {
+//  		console.log(age, `Вход восприщен.`);
+//  	}
+
+// 8==========================================================================================
+
+// let year = +prompt("Years at work");
+//     if (year > 0 && year < 5) {
+//  		console.log("your allowance is 0%");
+//     } else if (year >= 5 && year < 10) {
+//         console.log("your allowance is 5%");
+//     } else if (year >= 10 && year < 20) {
+//         console.log("your allowance is 10%");
+//     } else if (year >= 20 && year <= 40) {
+//         console.log("your allowance is 20%");
+//     } else if (year > 40) {
+//         console.log("you should be retired");
+//     } else {
+//         console.log("you not working here");
+//     }
+
+// 9==========================================================================================
+
+// function dayWeek(day) {
+// 	if (day === 1) {
+// 		console.log(`Monday`);
+// 	} else if (day === 2) {
+// 		console.log(`Tuesday`);
+// 	} else if (day === 3) {
+// 		console.log(`Wednesday`);
+// 	} else if (day === 4) {
+// 		console.log(`Thursday`);
+// 	} else if (day === 5) {
+// 		console.log(`Friday`);
+// 	} else if (day === 6) {
+// 		console.log(`Saturday`);
+// 	} else if (day === 7) {
+// 		console.log(`Sunday`);
+// 	} else {
+// 		console.log(`not have day like this`);
+// 	}
+// }
+
+// const day = +prompt(`wright day of the week`);
+// dayWeek(day);
+
+// switch case=======================
+
+// let day = +prompt("Number of day in the week");
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday ");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default :
+//         console.log("Not have day like this");
+//         break;
+// }
+
+//ternar===========
+
+// const day = +prompt("Number of day in the week");
+//     day === 1
+//         ? console.log("Monday")
+//         : day === 2
+//             ? console.log("Tuesday")
+//             : day === 3
+//                 ? console.log("Wednesday")
+//                 : day === 4
+//                     ? console.log("Thursday")
+//                     : day === 5
+//                         ? console.log("Friday")
+//                         : day === 6
+//                             ? console.log("Saturday")
+//                             : day === 7
+//                                 ? console.log("Sunday")
+//                                 : console.log("Not have day like this")
+
+// 10==========================================================================================
+
+// let number = +prompt(`wright number`);
+//    if (number < 0) {
+//        alert("-1");
+//    } else if ( number > 0) {
+//        alert("1");
+//    } else if ( number === 0) {
+//       alert("0");
+//    } else {
+//        alert("wright a number!");
+//    }
+
+// 11===========================================================================================
+
+// let login = prompt("Enter login");
+// login === "Сотрудник"
+//     ? message = "Привет"
+//     :  login === "Директор"
+//         ? message = "Здравствуйте"
+//         :  login === ""
+//             ? message = "Нет такого логина"
+//             : message =""
+// console.log(message)
+
+// 12===========================================================================================
+
+// function entrance(apartment) {
+// 	if (apartment >= 1 && apartment <=20) {
+// 		console.log('entrance 1');
+// 	} else if(apartment >=21 && apartment <=48 ) {
+// 		console.log('entrance 2');
+// 	} else if (apartment >= 49 && apartment <= 90){
+// 		console.log('entrance 3');
+// 	} else {
+// 		console.log('this house not have apartment like this')
+// 	}
+// }
+//
+// let apartment = +prompt('wright a number of apartment');
+// entrance(apartment);
+
+// 13===========================================================================================
+
+// let browser = prompt("Enter name of browser");
+// if (browser === "Edge") {
+//     alert("You've got the Edge!" );
+// } else if (browser ==="Chrome" || browser ==="Firefox" || browser ==="Safari" || browser ==="Opera") {
+//     alert('Okay we support these browsers too' );
+// } else {
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+// 14==========================================================================================
+
+// let number = +prompt('Введите число между 0 и 3', '');
+//
+// switch (number) {
+// case 0 :
+//     alert('Вы ввели число 0');
+//     break;
+// case 1:
+//     alert('Вы ввели число 1');
+//     break;
+// case 2:
+// case 3:
+//     alert('Вы ввели число 2, а может и 3');
+//     break;
+// default:
+//     alert("Выполните условие!");
+//     break;
+// }
